f71b917bd0329cdd1f3a1bd185d93362877ad19b
Chris Woodard <woodardc@goldmail.etsu.edu>
2018-02-04 17:07:41 -0500

    finalized the authentication and implemented some security measures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_same_user, only: [:edit, :update]
   # GET /users
   # GET /users.json
   def index
@@ -76,4 +76,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
+  
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
+    end
+        
 end
