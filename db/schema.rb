24a45ab93e823161f18162218d804bab4dbad539
Chris Woodard <woodardc@goldmail.etsu.edu>
2018-02-04 00:08:15 -0500

    passwords added for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204030102) do
+ActiveRecord::Schema.define(version: 20180204045918) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180204030102) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
