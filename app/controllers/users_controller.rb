0d0b0eb20ea84d7bc30e69119eb8c4957e3c55a8
Chris Woodard <woodardc@goldmail.etsu.edu>
2018-02-04 18:51:43 -0500

    user type and basic administrative function implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :require_admin, only: [:destroy]
   # GET /users
   # GET /users.json
   def index
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         flash[:success] = "Welcome to the blog #{@user.username}!"
         format.html { redirect_to @user }
         format.json { render :show, status: :created, location: @user }
@@ -59,9 +61,11 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = set_user
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to articles_path, notice: 'User was successfully destroyed.' }
+      flash[:danger] = "The user and all of its articles have been successfully deleted."
+      format.html { redirect_to users_path}
       format.json { head :no_content }
     end
   end
@@ -83,5 +87,11 @@ class UsersController < ApplicationController
         redirect_to root_path
       end
     end
-        
+  
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "Only admin users can perform that action"
+        redirect_to root_path
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204045918) do
+ActiveRecord::Schema.define(version: 20180204230914) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180204045918) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
