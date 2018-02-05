1da84976e20025b44811ab50e51db2735c9b196c
Chris Woodard <woodardc@goldmail.etsu.edu>
2018-02-05 08:39:51 -0500

    categories may only be added by admins

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 require 'active_support/core_ext'
 class CategoriesController < ApplicationController
-  
+  before_action :require_admin, except: [:index, :show]
   def setup
     @category = Category.new(name: "turtles")
   end
@@ -33,6 +33,13 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+  
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Admin Only"
+      redirect_to categories_path
+    end
   end
   
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   
   def setup
-     @category = Category.new(id: 0, name: "turtles")    
+     @category = Category.new(id: 0, name: "turtles")  
+     @user = User.create(username: "joey", email: "joeys@world.com", password: "password", admin:true)
   end
   
   test "shouldl get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -19,5 +21,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  
+  test "should redirect page from create when user is not an administrator" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "turtles"}
+    end
+    assert_redirect_to categories_path
   end
 end
